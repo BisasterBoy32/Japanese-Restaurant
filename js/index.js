2d90ac900e115ba849678c26a66c00a02363fa3c
mohamed belkahla <mohamadbr22@gmail.com>
2019-09-13 10:35:50 +0100

    c1

---
+++ js/index.js
@@ -0,0 +1,60 @@
+/***********************
+        scroll
+ ***********************/
+
+window.addEventListener('scroll', function () {
+
+    if (window.scrollY >= 100) {
+        document.querySelector('header')
+            .classList.add('scrolled')
+        
+    } else if (window.scrollY < 100) {
+        document.querySelector('header')
+            .classList.remove('scrolled')
+        console.log(scrollY)
+    }
+
+})
+
+/************************
+    smooth navigation
+ ************************/
+$(document).ready(function () {
+    $(".custom-a").on("click", function (e) {
+        e.preventDefault();
+        $('.humIcon').click()
+        let section = $(this).attr("href")
+        $('html ,body').animate({
+            scrollTop: $(section).offset().top - 120
+        }, 1200, "easeInOutExpo")
+    })
+})
+
+const links = document.querySelectorAll('.custom-a')
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function (e) {
+        for (let i = 0; i < links.length; i++) {
+            links[i].classList.remove('current')
+        }
+        this.classList.add('current')
+    })
+}
+
+/************************
+        HumbergerMenu
+************************/
+document.querySelector(".humIcon")
+    .addEventListener("click", function () {
+        document.querySelector(".iconRight")
+            .classList.toggle("active")
+        document.querySelector("header")
+            .classList.toggle("expand")
+    })
+
+
+/********************
+        WOW JS
+*********************/
+$(document).ready(function(){
+    new WOW().init();
+})
